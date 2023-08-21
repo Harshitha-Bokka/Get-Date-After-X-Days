a3eacc25952896589e2717daa4c8016dee76391c
Harshitha-Bokka <bokkaharshitha7@gmail.com>
2023-08-21 10:54:59 +0000

    Get Date After X Days

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const addDays = require("date-fns/package");
-const day=(days)=>{
-    const result=addDays(new Date(2020,7,22),days);
-    return `${result.getDate()}-${result.getMonth()+1}-${result.getFullYear()}`;
+const day = (days) => {
+  const result = addDays(new Date(2020, 7, 22), days);
+  return `${result.getDate()}-${result.getMonth() + 1}-${result.getFullYear()}`;
 };
-export.modules=day;
+module.exports = day;
